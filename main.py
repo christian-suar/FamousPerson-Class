88f9d32404fb3262d7210d99ea42a2450d11f80a
christian-suar <148103464+christian-suar@users.noreply.github.com>
2025-03-21 10:26:57 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Create a FamousPerson class with:
+Attributes: name, birth_year, country_of_birth
+Method: introduce()
+Example format: [name] was born in [birth_year] in [country_of_birth]
+Create subclasses:
+Scientist (adds field, notable_discovery)
+Politician (adds position, years_in_office)
+NOTE: you will need a new introduce() for each subclass so the printed statement will include the additional information for each.
+Create instances for Albert Einstein,  Lebron James, and Abraham Lincoln, then call introduce() for each of them. 
+Remember to use the correct class or subclass when you create these famouse people.
+"""
